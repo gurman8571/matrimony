d3dc5bee3eb70f596ce264cd12a3d02142211b4e
“gurman8571” <gurman@8571@gmail.com>
2022-06-21 20:41:06 +0530

    bug fixes

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Blogs;
+
 class BlogController extends Controller
 {
-   public function create(Type $var = null)
-   {
-       //
-   }
+    //
 }
